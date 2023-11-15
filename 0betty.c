fcf95de9d182fca59920a56e5c56631f7b0a6a38
yenaossai <yenaossai@gmail.com>
2023-11-15 22:39:56 +0000

    update

--- 0betty.c
+++ 0betty.c
@@ -4,12 +4,14 @@
  * main - betty checks
  *
  * Description: prints a greeting message
+ *
  * Return: 0 (success)
  */
 
 int main(void)
 {
 	const char message[] = "Hello, Beautiful Betty!\n";
+
 	write(1, message, sizeof(message) - 1);
 
 	return (0);
